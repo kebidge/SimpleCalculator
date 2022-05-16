9a1709ac9a6432f95630286d29fa2ac10b7727e2
Kevin Kutzner <kevin.kutzner@hft-stuttgart.de>
2022-05-16 19:14:00 +0200

    fix

--- src/main/java/hftstuttgart/Calculator.java
+++ src/main/java/hftstuttgart/Calculator.java
@@ -12,9 +12,9 @@ public class Calculator {
 
     public int prod(int op1, int op2) {
         int sum = 0;
-        //sum = op1 * op2;
-        sum *= op1;
-        sum *= op2;
+        sum = op1 * op2;
+        //sum *= op1;
+        //sum *= op2;
         return sum;
     }    
 }
